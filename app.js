1b4d3d1d48f7aff606d01373d33a1db527c04a66
Jawascript <n.jmonplaisir@gmail.com>
2021-03-31 15:51:57 -0400

    adding assets

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+let teamMembers = [];
 
 function createNewMember() {
     inquirer
